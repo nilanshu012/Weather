16bdd7c379fac43976d6b6bce862e05b4b455777
Nil <nil@local.com>
2023-09-02 01:49:41 +0530

    Changes made to deploy

--- index.js
+++ index.js
@@ -1,42 +1,40 @@
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
-import 'dotenv/config';
+import "dotenv/config";
 
-const app= express();
-const port =3000;
-const apiKey=process.env.API_KEY;
+const app = express();
+const port = process.env.PORT || 3000;
+const apiKey = process.env.API_KEY;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
 
-app.get("/",(req,res)=>{
+app.post("/city", async (req, res) => {
+  const searchCity = req.body.city;
+
+  const searchUrl =
+    "http://api.weatherapi.com/v1/current.json?key=" +
+    apiKey +
+    "&q=" +
+    searchCity +
+    "&aqi=yes";
+  console.log(searchUrl);
+
+  try {
+    const result = await axios.get(searchUrl);
+    res.render("index.ejs", { data: result.data });
+    console.log(result.data);
+  } catch (error) {
+    console.log(error);
     res.render("index.ejs");
-})
-
-app.post("/city",async(req,res)=>{
-
-    const searchCity=req.body.city;
-
-    const searchUrl = "http://api.weatherapi.com/v1/current.json?key="+apiKey+"&q="+searchCity+"&aqi=yes"
-    console.log(searchUrl);
-
-    try{
-        const result =await axios.get(searchUrl);
-        //res.render("index.ejs");
-        res.render("index.ejs",{data:result.data});
-        console.log(result.data);
-    }catch (error) {
-           console.log(error);
-           res.render("index.ejs")
-        //    alert("Invalid City Name !!!")
-           }
-
-    
-
-})
+  }
+});
 
 app.listen(port, () => {
-    console.log(`Server running on port: ${port}`);
-  });
+  console.log(`Server running on port: ${port}`);
+});
